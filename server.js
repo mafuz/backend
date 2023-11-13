daa2ab1cee0e1cb0aa1ac2f711c91553a714d3ee
mafuz <mmutarum@gmail.com>
2023-11-13 18:06:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ const salt = 10;
 //middleware
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(express.json());

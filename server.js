b57cc3aaea0f0d3236ba053cef5ebf560d8e1d46
mafuz <mmutarum@gmail.com>
2023-11-13 18:15:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,11 +18,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
-// app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(express.json());
 

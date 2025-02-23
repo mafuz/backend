ffaaff887cf794322859d1cc225897fb78299b86
mafuz <mmutarum@gmail.com>
2025-02-23 09:27:41 +0100

    Update database.js

--- database.js
+++ database.js
@@ -2,14 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
-  password: process.env.DATABASE_PASSWORD,
-  user: process.env.DATABASE_USER,
-  port: process.env.DATABASE_PORT,
+
   connectionString: process.env.DATABASE_URL,
 });
 
-
+ # database: process.env.DATABASE_NAME,
+ # host: process.env.DATABASE_HOST,
+#  password: process.env.DATABASE_PASSWORD,
+#  user: process.env.DATABASE_USER,
+ # port: process.env.DATABASE_PORT,
 
 module.exports = pool;

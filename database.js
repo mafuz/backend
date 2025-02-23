f5a081c9fb5e074f7d7e9aca895a557faed97163
mafuz <mmutarum@gmail.com>
2025-02-23 09:33:12 +0100

    Update database.js

--- database.js
+++ database.js
@@ -6,10 +6,6 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
- # database: process.env.DATABASE_NAME,
- # host: process.env.DATABASE_HOST,
-#  password: process.env.DATABASE_PASSWORD,
-#  user: process.env.DATABASE_USER,
- # port: process.env.DATABASE_PORT,
+
 
 module.exports = pool;
